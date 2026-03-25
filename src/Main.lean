e97e2165b1057ef635f1799b42e1d01cf20e8528
Stefan Alfbo <stefan@alfbo.se>
2026-03-25 20:09:45 +0100

    Implement file reading functionality in main entry point

--- src/Main.lean
+++ src/Main.lean
@@ -1,4 +1,11 @@
 import LeanForth
 
-def main : IO Unit :=
-  IO.println "LeanForth"
+def printFileLines (path : System.FilePath) : IO Unit := do
+  let contents ← IO.FS.readFile path
+  for line in contents.splitOn "\n" do
+    IO.println line.trimAsciiEnd.toString
+
+def main (args : List String) : IO Unit := do
+  match args with
+  | filePath :: _ => printFileLines filePath
+  | [] => IO.println "Usage: leanforth.exe <file>"
